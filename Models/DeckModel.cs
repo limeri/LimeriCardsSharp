d6e624ba949021a6ea4b9a806aaf3a2f548cb473
limeri <andylimeri@yahoo.com>
2026-03-26 09:04:47 -0400

    Remove unnecessary class

--- Models/DeckModel.cs
+++ Models/DeckModel.cs
@@ -99,11 +99,3 @@ namespace LimeriCardsSharp.Models
         }
     }
 }
-
-namespace LimeriCardsSharp.Models
-{
-    public class DeckModel
-    {
-        public List<CardModel> Cards { get; set; }
-    }
-}
